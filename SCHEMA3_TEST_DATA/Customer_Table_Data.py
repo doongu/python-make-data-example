bb843f0327b950c79fa78eb468d080abe491d249
doongu <must1080@naver.com>
2023-07-01 18:21:52 +0900

    SCHEMA3 Customer_Table_Data 작성 완료

--- SCHEMA3_TEST_DATA/Customer_Table_Data.py
+++ SCHEMA3_TEST_DATA/Customer_Table_Data.py
@@ -1,16 +1,21 @@
 from faker import Faker
 import os 
 
-tuple_length = int(input("Customer_Table의 데이터를 생성합니다. \n\n 튜플의 갯수를 입력해주세요 : "))
+CASE = int(input("Customer_Table의 데이터를 생성합니다 CASE를 입력해주세요"))
 file_name = input("파일 이름을 작성해 주세요 : ")
 
-def make_data_file(tuple_length, file_name):
+
+def make_data_file(CASE, file_name):
+    CASES = {1 : 1000, 2 : 100, 3 : 10, 4 : 1,
+             5 : 1000, 6 : 100, 7 : 10, 8 : 1}
+
+
     path = os.path.dirname(os.path.abspath(__file__))
 
     fake = Faker()    
     fd = open(path + '/data_files/' + file_name + '.txt', 'w')
 
-    for key in range(1, tuple_length + 1):
+    for key in range(1, CASES[CASE] + 1):
         name_split = fake.name().split()
         name_sum = ''.join(name_split)
 
@@ -18,4 +23,4 @@ def make_data_file(tuple_length, file_name):
     else:
         fd.close()
 
-make_data_file(tuple_length, file_name)
+make_data_file(CASE, file_name)
