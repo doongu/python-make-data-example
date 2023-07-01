9d9d0034fcc6beb6449d113a0f260bb9f2f8f954
doongu <must1080@naver.com>
2023-07-01 16:11:35 +0900

    이름 공백 처리

--- SCHEMA1_TEST_DATA/Customer_Table_DATA.py
+++ SCHEMA1_TEST_DATA/Customer_Table_DATA.py
@@ -1,7 +1,7 @@
 from faker import Faker
 import os 
 
-tuple_length = int(input("튜플의 갯수를 입력해주세요 : "))
+tuple_length = int(input("Customer_Table의 데이터를 생성합니다. \n\n 튜플의 갯수를 입력해주세요 : "))
 file_name = input("파일 이름을 작성해 주세요 : ")
 
 def make_data_file(tuple_length, file_name):
@@ -9,10 +9,12 @@ def make_data_file(tuple_length, file_name):
 
     fake = Faker()    
     fd = open(path + '/data_files/' + file_name + '.txt', 'w')
-    # fd = open(file_name, 'w')
 
     for key in range(1, tuple_length + 1):
-        fd.write(str(key) + ' ' + fake.name() + ' ' + fake.email() + '\n')
+        name_split = fake.name().split()
+        name_sum = ''.join(name_split)
+
+        fd.write(str(key) + ' ' + name_sum + ' ' + fake.email() + '\n')
     else:
         fd.close()
 
